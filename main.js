0d4fb8b680fada4c621905b88e250e4a08399ea4
Mr Speaker <mrspeaker@gmail.com>
2022-08-09 09:20:07 +0800

    nibs

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 const mk_mem = (size) => [...Array(size)].fill(0);
-const mk_regs = (num) =>
-  [...Array(num)].fill(0).map((r) => [0, 0, 0, 0, 0, 0, 0, 0]);
+const mk_regs = (num) => [...Array(num)].fill(0).map((r) => [0, 0, 0, 0]);
 
 const loadTxtObj = async (path) => {
   const obj = await fetch(path)
@@ -12,13 +11,15 @@ const loadTxtObj = async (path) => {
     .replace(/\n/g, " ")
     .split(" ")
     .filter((b) => b.trim())
-    .map((b) => b.toString(16));
+    .map((b) => b.toString(16))
+    .map((b) => parseInt(b, 16));
 
   return bytes;
 };
 
 (async function main() {
   const bytes = await loadTxtObj("/a.obj");
+  console.log(bytes);
   run(bytes);
 })();
 
@@ -26,48 +27,80 @@ function run(bytes) {
   const env = {
     pc: 0,
     regs: mk_regs(15),
-    mem: mk_mem(100),
+    mem: mk_mem(4096),
   };
+  env.mem[10] = 0xff;
+
   env.pc = step(bytes, env);
   env.pc = step(bytes, env);
   env.pc = step(bytes, env);
   env.pc = step(bytes, env);
 }
 
+const nib = (byte) => [byte >> 4, byte % 16];
+const nib_hi = (byte) => [];
+const nib_lo = (byte) => [];
+const byte_from = (nib1, nib2) => (nib1 << 4) + nib;
+const nib3_to_byte = (nib1, nib2, nib3) => (nib1 << 8) + (nib2 << 4) + nib3;
+
+const nibs = (bytes, ...lengths) => {
+  const nibs = bytes.map(nib).flat();
+  return lengths.reduce((ac, len) => {
+    //
+    return ac;
+  }, []);
+};
+
 function step(bytes, env) {
-  let idx = env.pc;
-  const op = bytes[idx++];
+  const { regs, mem } = env;
+  let { pc } = env;
+  const op = bytes[pc++];
   let opers;
+  let r1, r2, r3, b2, d1, d2, d3;
+  let d;
+
   switch (op) {
-    case "07":
+    case 0x07:
       //BCR M1,R2 [RR] - 2 bytes
       //0:'07' 8: M1 12: R2
-      opers = bytes[idx++];
+      opers = bytes[pc++];
       console.log("BCR", opers);
       break;
-    case "5a":
+    case 0x5a:
       // A R1,D2(X2,B2) [rx] - 4 bytes
       // 0:"5a" 8:r1 12:x2, 16:b2, 20:d2
-      opers = bytes.slice(idx, idx + 3);
-      idx += 3;
+      opers = bytes.slice(pc, pc + 3);
+      pc += 3;
       console.log("A", opers);
       break;
-    case "98":
+    case 0x98:
       // LM R1,R3,D2(b2) - 4bytes
       // 0:"98" 8:r1 12:r3 16:b2 20:d2
-      opers = bytes.slice(idx, idx + 3);
-      idx += 3;
-      console.log("LM", opers);
+      // load from r1 to r3 from storage addr.
+      opers = bytes
+        .slice(pc, pc + 3)
+        .map(nib)
+        .flat();
+      pc += 3;
+      [r1, r2, r3, d1, d2, d3] = opers;
+      d = nib3_to_byte(d1, d2, d3);
+
+      let ptr = regs[r3][3] + d;
+      regs[r1][3] = mem[ptr++];
+      regs[r2][3] = mem[ptr++];
+      regs[r3][3] = mem[ptr++];
+
+      console.log("LM", regs[r1], regs[r2], regs[r3]);
 
       break;
-    case "0d":
+    case 0x0d:
       // BSR - 2bytes
-      const r1r2 = bytes[idx++];
+      const r1r2 = bytes[pc++];
       console.log("BSR", r1r2);
 
       break;
     default:
-      console.log("wat op?", op);
+      console.log("wat op?", op, op.toString(16));
   }
-  return idx;
+  return pc;
 }
