669c9b285c83fcc26480e14e73ec73cc0d283918
Mr Speaker <mrspeaker@gmail.com>
2022-08-21 22:12:45 +0800

    bit o style

--- src/main.js
+++ src/main.js
@@ -1,35 +1,27 @@
-import { bindAssembleUI, assembleText } from "./assemble.js";
-import {
-  $,
-  $click,
-  eb2asc,
-  toHex,
-  memcpy,
-  chkBytes,
-  formatObjRecord,
-  loadTxtObj,
-} from "./utils.js";
-
+import render from "./render.js";
 import actionReducer from "./actionReducer.js";
-import { mk_state } from "./state.js";
 import { run } from "./emulate.js";
-import { disassemble } from "./disassemble.js";
+import { bindAssembleUI, assembleText } from "./assemble.js";
+import { mk_state } from "./state.js";
+import { $, $click, eb2asc, loadTxtObj } from "./utils.js";
 
 const state = mk_state();
 const action = actionReducer(state, render);
 
-function render(state) {
-  const { env, obj, code, goff, showObjBytes } = state;
-  $("#format").innerText = goff ? "GOFF" : "OBJ";
-  $("#obj").value = obj.map(formatObjRecord).join("\n\n");
-  $("#regs").value = env.regs.map((v) => v.map((x) => toHex(x))).join("\n");
-  $("#mem").value = env.mem.map((m) => toHex(m));
-  $("#emu").value = env.code_txt?.join("\n");
-  $("#dis").value = disassemble(code, showObjBytes).join("\n");
-  $("#psw_cc").value = env.psw.conditionCode;
-}
-
 (async function main(state) {
+  const obj = await loadTxtObj("../obj/max.o");
+  action("LOAD_OBJ", { obj });
+
+  state.obj.forEach((o, i) => {
+    const [_, a, b, c] = o;
+    console.log(i, [a, b, c].map((c) => eb2asc(c)).join(""));
+  });
+
+  action("RUN");
+  bindUI(state, action);
+})(state, action);
+
+function bindUI(state, action) {
   bindAssembleUI((obj) => {
     action("LOAD_OBJ", { obj });
   });
@@ -51,14 +43,4 @@ function render(state) {
   });
 
   $click("#btnRun", () => action("RUN"));
-
-  const obj = await loadTxtObj("../obj/max.o");
-  action("LOAD_OBJ", { obj });
-
-  state.obj.forEach((o, i) => {
-    const [_, a, b, c] = o;
-    console.log(i, [a, b, c].map((c) => eb2asc(c)).join(""));
-  });
-
-  action("RUN");
-})(state, action);
+}
