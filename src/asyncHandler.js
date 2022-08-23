0e4f509869c942b87b934cb7cf38c924dfcaedf9
Earle Castledine <earlec@21csw.com>
2022-08-23 18:24:22 +0800

    lol tryerr

--- src/asyncHandler.js
+++ src/asyncHandler.js
@@ -1,12 +1,24 @@
 import { load_prog, mk_program_from_obj } from "./program.js";
 
+const tryerr =
+  (f) =>
+  async (...args) => {
+    try {
+      return [null, await f(...args)];
+    } catch (err) {
+      return [err, []];
+    }
+  };
+
 const asyncHandler = (action) => async (type, value) => {
   switch (type) {
     case "PROG_LOAD":
       {
-        const { obj, src } = await load_prog(value);
-        const prog = mk_program_from_obj(obj, src);
-        action("PROG_LOADED", prog);
+        const [err, { obj, src }] = await tryerr(load_prog)(value);
+        if (!err) {
+          const prog = mk_program_from_obj(obj, src);
+          action("PROG_LOADED", prog);
+        }
       }
       break;
     default:
