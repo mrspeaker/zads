1771fd645014d1ec61124371c09e187f73e097e8
Mr Speaker <mrspeaker@gmail.com>
2022-08-31 08:08:02 +0800

    Binder helper

--- src/bind.js
+++ src/bind.js
@@ -1,20 +1,11 @@
 export const bind = (bytes) =>
   [
-    2,
-    227,
-    231,
-    227,
-    0x40,
-    0x40,
-    0x40,
-    0x40,
-    0x40,
-    0x40,
-    0x40,
+    ...header_id,
+    ...fill(4, 10),
     bytes.length,
-    0x40,
-    0x40,
-    0x40,
-    0x40,
+    ...fill(12, 15),
     ...bytes,
   ].flat();
+
+const header_id = [2, 227, 231, 227]; // 2, TXT
+const fill = (from, to) => [...Array(to - from)].fill(0x40);
