9b2138648eea1ea9eaee95d67afacaeb86accdd0
Mr Speaker <mrspeaker@gmail.com>
2022-08-31 08:14:40 +0800

    label header

--- src/bind.js
+++ src/bind.js
@@ -1,11 +1,11 @@
 export const bind = (bytes) =>
   [
-    ...header_id,
+    ...header_0_3,
     ...fill(4, 10),
     bytes.length,
     ...fill(12, 15),
     ...bytes,
   ].flat();
 
-const header_id = [2, 227, 231, 227]; // 2, TXT
+const header_0_3 = [2, 227, 231, 227]; // 2, TXT
 const fill = (from, to) => [...Array(to - from)].fill(0x40);
