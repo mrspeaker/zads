860f073cb55f8a154fca809a4a9d2db88d6c709a
Mr Speaker <mrspeaker@gmail.com>
2022-09-12 07:48:18 +0800

    output tests

--- tests/index.js
+++ tests/index.js
@@ -4,23 +4,30 @@ import { ops, op_by_mn } from "../src/ops.js";
 
 const dis_lr = () => {
   const bytes = disassemble([0x18, 0x12], {}, false);
-  console.log("00: LR 1.2", bytes);
+  return bytes[0] === "00: LR 1.2";
 };
 
 const op_lr = () => {
   const env = { regs: [[0, 0, 0, 0], [0, 0, 0, 1]], psw: { pc: 0 } };
   const op = ops[op_by_mn["LR"]];
   const obj = [...op.code, 1];
-  const out = step(obj, env);
-  console.log(out, JSON.stringify(env));
+  step(obj, env);
+  return env.regs[0][3] === 1;
 };
 
 const tests = () => {
-  dis_lr();
-  op_lr();
+  return [dis_lr, op_lr].map((f) => {
+    const name = f.name;
+    const passed = !!f();
+    return { name, passed };
+  });
 };
 
 function main() {
-  tests();
+  tests().forEach(({ name, passed }) => {
+    const div = document.createElement("div");
+    div.innerText = `${name}: ${passed ? "OK" : "FAIL"}`;
+    document.body.appendChild(div);
+  });
 }
 main();
