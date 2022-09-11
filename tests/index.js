0ed9592261f49eefba00b54077c16261b05c3cd3
Mr Speaker <mrspeaker@gmail.com>
2022-09-12 07:40:38 +0800

    test

--- tests/index.js
+++ tests/index.js
@@ -1,4 +1,26 @@
 import { disassemble } from "../src/disassemble.js";
+import { step } from "../src/emulate.js";
+import { ops, op_by_mn } from "../src/ops.js";
 
-const bytes = disassemble([0x18, 0x12], {}, false);
-console.log("00: LR 1.2", bytes);
+const dis_lr = () => {
+  const bytes = disassemble([0x18, 0x12], {}, false);
+  console.log("00: LR 1.2", bytes);
+};
+
+const op_lr = () => {
+  const env = { regs: [[0, 0, 0, 0], [0, 0, 0, 1]], psw: { pc: 0 } };
+  const op = ops[op_by_mn["LR"]];
+  const obj = [...op.code, 1];
+  const out = step(obj, env);
+  console.log(out, JSON.stringify(env));
+};
+
+const tests = () => {
+  dis_lr();
+  op_lr();
+};
+
+function main() {
+  tests();
+}
+main();
