231ce60de250a459d3917b88d1c742ac9479d7eb
Mr Speaker <mrspeaker@gmail.com>
2022-09-12 08:14:04 +0800

    clean up test

--- tests/index.js
+++ tests/index.js
@@ -1,6 +1,7 @@
 import { disassemble } from "../src/disassemble.js";
 import { step } from "../src/emulate.js";
 import { ops, op_by_mn } from "../src/ops.js";
+import { byte_from, regval, fw_to_bytes } from "../src/bytes.js";
 
 const dis_lr = () => {
   const bytes = disassemble([0x18, 0x12], {}, false);
@@ -8,11 +9,20 @@ const dis_lr = () => {
 };
 
 const op_lr = () => {
-  const env = { regs: [[0, 0, 0, 0], [0, 0, 0, 1]], psw: { pc: 0 } };
+  const env = {
+    regs: [fw_to_bytes(10), fw_to_bytes(42)],
+    psw: { pc: 0, conditionCode: 0 },
+  };
   const op = ops[op_by_mn["LR"]];
-  const obj = [...op.code, 1];
+  const obj = [...op.code, byte_from(0, 1)];
+
   step(obj, env);
-  return env.regs[0][3] === 1;
+
+  const { regs, psw } = env;
+  const [r0, r1] = regs;
+  return (
+    regval(r1) === 42 && regval(r0) === regval(r1) && psw.conditionCode === 0
+  );
 };
 
 const tests = () => {
