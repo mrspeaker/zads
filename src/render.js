c5dad2710243e34eb023cf64796ecf247eaf8afe
Earle Castledine <earlec@21csw.com>
2022-09-16 12:32:56 +0800

    lol viz

--- src/render.js
+++ src/render.js
@@ -2,7 +2,7 @@ import { disassemble } from "./disassemble.js";
 import { vic_regs, pal_to_rgb, updateVic } from "./vic.js";
 
 import { chunk, $, toHex, formatObjRecord } from "./utils.js";
-import { memval, bytes_to_fw } from "./bytes.js";
+import { memval, to_nibs, bytes_to_fw } from "./bytes.js";
 
 function render(state) {
   const { machine, program, zads, programs, selected, cyclesPerFrame } = state;
@@ -82,15 +82,16 @@ function renderMemViz(mem) {
   const c = $("#memviz").getContext("2d");
   const imgData = c.getImageData(0, 0, c.canvas.width, c.canvas.height - 10);
   imgData.data.forEach((d, i) => {
-    const v = mem[i] === 0 ? 0 : (((mem[i] / 255) * 15) | 0) + 1;
-    const c = pal_to_rgb(v)
+    //const v = mem[i] === 0 ? 0 : (((mem[i] / 255) * 15) | 0) + 1;
+    const v = to_nibs(mem[i]);
+    const c = pal_to_rgb(v[1] === 0 ? v[0] : v[1])
       .split("")
       .slice(1)
       .map((h) => parseInt(h, 16) * 16);
     imgData.data[i * 4] = c[0];
     imgData.data[i * 4 + 1] = c[1];
     imgData.data[i * 4 + 2] = c[2];
-    imgData.data[i * 4 + 3] = 255;
+    imgData.data[i * 4 + 3] = 255; //(vv[1]/16)*255;
   });
   c.putImageData(imgData, 0, 0);
 }
