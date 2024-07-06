65c8652e2ae81a9c6791c04e4ad8f39ee0fe4a1b
Mr Speaker <mrspeaker@gmail.com>
2024-07-07 07:12:11 +0800

    mouse move draw

--- src/ui_sprites.js
+++ src/ui_sprites.js
@@ -9,16 +9,49 @@ function init_tile(state, action) {
   let w = ctx.canvas.width;
   let h = ctx.canvas.height;
 
-  $click(ctx.canvas, (e) => {
-    const { sprite_data, cur_sprite, spr_w, spr_h } = state;
+  const draw_pixel = (tx, ty) => {
+    const { sprite_data, cur_sprite, spr_w } = state;
     const spr = sprite_data[cur_sprite];
+    const idx = ty * spr_w + tx;
+    spr[idx] = spr[idx] ? 0 : 1;
+    action("TILE_UPDATE", [...spr]);
+  };
 
+  const get_tile = (e) => {
+    const { spr_w, spr_h } = state;
     const { x, y } = $get_ev_pos(e);
     const tx = ((x / w) * spr_w) | 0;
     const ty = ((y / h) * spr_h) | 0;
-    const idx = ty * spr_w + tx;
+    return { tx, ty };
+  };
 
-    spr[idx] = spr[idx] ? 0 : 1;
-    action("TILE_UPDATE", [...spr]);
+  let is_down = false;
+  let last_x = -1;
+  let last_y = -1;
+  let init_tx = -1;
+  let init_ty = -1;
+  $on(ctx.canvas, "mousedown", (e) => {
+    is_down = true;
+    const { tx, ty } = get_tile(e);
+    init_tx = tx;
+    init_ty = ty;
+  });
+
+  $on(ctx.canvas, "mouseup", (e) => {
+    is_down = false;
+    const { tx, ty } = get_tile(e);
+    if (init_tx == tx && init_ty === ty) {
+      draw_pixel(tx, ty);
+    }
+  });
+
+  $on(ctx.canvas, "mousemove", (e) => {
+    if (!is_down) return;
+    const { tx, ty } = get_tile(e);
+    if (tx !== last_x || ty !== last_y) {
+      last_x = tx;
+      last_y = ty;
+      draw_pixel(tx, ty);
+    }
   });
 }
